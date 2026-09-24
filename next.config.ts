589fc0f66cdc7a4e09c162023e28f8fe01ad84c7
Beleza Nativa <isamarasilva850@gmail.com>
2026-09-24 16:52:10 -0300

    Fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  reactStrictMode: true,
-  onDemandEntries: {
-    maxInactiveAge: 60 * 1000,
-    pagesBufferLength: 5,
-  },
 };
 
 export default nextConfig;
